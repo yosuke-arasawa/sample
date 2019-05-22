c0108ac5e3b5a4b21c27a992e73f53be48513706
yosuke-arasawa <50505457+yosuke-arasawa@users.noreply.github.com>
2019-05-22 16:33:28 +0900

    js実装

---
+++ js/script.js
@@ -0,0 +1,59 @@
+﻿$(function () {
+
+
+
+
+    //スクロールアニメーション
+    //トップ
+    $('.logo-left').click(function () {
+
+        $('html,body').animate({
+            'scrollTop': 0
+        }, 'slow');
+
+    });
+
+    //スクロール
+    //各ページ
+    $('.btn').click(function () {
+
+        var id = $(this).attr('href');
+        var position = $(id).offset().top;
+        $('html,body').animate({
+            'scrollTop': position - 100
+        }, 'slow');
+
+    });
+
+
+
+
+
+
+    //ホバー時ヘッダーフォント
+    $('header a').hover(
+        function () {
+
+            $(this).animate({
+                'font-size': '17px'
+            }, 100);
+
+        },
+        function () {
+
+            $(this).animate({
+                'font-size': '14px'
+            }, 300);
+
+        }
+    );
+
+
+
+
+
+
+
+
+
+});
