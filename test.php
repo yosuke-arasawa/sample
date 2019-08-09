8c951782d0cb7e9e80c80d80a4e81654a8fcedd0
arasawa yosuke <arasawa@arnk.local>
2019-08-09 13:58:03 +0900

    20190809アンク退職日

---
+++ test.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+</head>
+
+<body>
+<?php
+
+$title = $_POST['title'];
+$body = $_POST['body'];
+
+?>
+
+    <h1><?php echo $title ?></h1>
+
+</body>
+</html>
